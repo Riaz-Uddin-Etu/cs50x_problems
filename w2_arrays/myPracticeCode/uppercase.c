f2c1e1d0697c10f0ba338205f1aa1ebf8c6a0cf4
Riaz-Uddin-Etu <riazuddinetu23@gmail.com>
2025-11-10 18:26:04 +0600

    update: more simplify form using ctype function

--- w2_arrays/myPracticeCode/uppercase.c
+++ w2_arrays/myPracticeCode/uppercase.c
@@ -9,14 +9,7 @@ int main(void)
 
     for (int i = 0, n = strlen(name); i < n; i++)
     {
-        if (islower(name[i]))
-        {
-            printf("%c", toupper(name[i]));  // convert to each character -- so %c
-        }
-        else
-        {
-            printf("%c", name[i]);
-        }
+        printf("%c", toupper(name[i]));
     }
     printf("\n");
 }
